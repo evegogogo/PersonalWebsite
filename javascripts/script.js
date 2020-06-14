8625e31d1e13a41c61d8253d47e5bf51346e9381
shenme11 <shen.me@husky.neu.edu>
2020-06-13 19:35:53 -0700

    finish the website design

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,7 +1,27 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    document.getElementById("div2").textContent="Document DOCContentLoaded event happend!";
+$(document).ready(function () {
+    $(".lightbox_trigger").click(function (e) {
+        if ($(window).width() > 812) {
+            // Disable if on Mobile
+            // Given it is unnecessary to have enlarged
+            // view of image 
+            e.preventDefault();
+            var image_src = $(this).attr("src")
+            if ($(".lightbox").length > 0) {
+                $(".content").html('<img src="' + image_src + '" />');
+                $(".lightbox").show();
+            } else {
+                var lightbox =
+                    '<div class="lightbox">' +
+                    '<p>Click to close</p>' +
+                    '<div class="content">' +
+                    '<img src="' + image_src + '" />' +
+                    '</div>' +
+                    '</div>';
+                $('body').append(lightbox);
+            }
+        }
+    });
+    $('body').on('click', '.lightbox', function () {
+        $('.lightbox').hide();
+    });
 });
-
-window.onload = function() {
-    document.getElementById("div1").textContent = "Window.onload event happened!";
-}
